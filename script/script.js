694d1b6af1928d84b99e40db841857ada168e6c8
BilalZouine <144604270+BilalZouine@users.noreply.github.com>
2024-05-03 22:50:58 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -275,7 +275,7 @@ function ajouter() {
         document.querySelector('#error2').innerText = ''
         cheked++
     }
-    if (!patern_password.test(pass) &&  pass.length >=8) {
+    if (!patern_password.test(pass) ||  pass.length >=8) {
         document.querySelector('#error5').innerText = 'Vérifier vote mot de pass  Minimum 8 caractères*'
     } else {
         document.querySelector('#error5').innerText = ''
